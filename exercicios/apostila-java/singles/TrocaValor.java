7eef60261f03a2ba3f918dadb33564d9ae258aee
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-03-07 22:54:17 -0300

    exercício trocaValor

---
+++ exercicios/apostila-java/singles/TrocaValor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TrocaValor {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("A:");
+    int a = ler.nextInt();
+
+    System.out.println("B:");
+    int b = ler.nextInt();
+
+    int aux = a;
+    a = b;
+    b = aux;
+
+    System.out.printf("A: %d B: %d", a, b);
+  }
+}
